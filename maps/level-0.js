b9dfa02cfecb2ca25a158c12abd80ce7a5d548d4
Tania Rascia <taniarascia@gmail.com>
2021-11-03 22:14:43 -0500

    Init

--- maps/level-0.js
+++ maps/level-0.js
@@ -3,21 +3,21 @@ import { nounTypes, actionTypes, connectorTypes } from '../constants.js'
 
 const entities = []
 const textEntities = [
-  new TextEntity(nounTypes.BABA, { x: 6, y: 6 }),
-  new TextEntity(connectorTypes.IS, { x: 7, y: 6 }),
-  new TextEntity(actionTypes.YOU, { x: 8, y: 6 }),
+  new TextEntity(nounTypes.BABA, { x: 4, y: 7 }),
+  new TextEntity(connectorTypes.IS, { x: 5, y: 7 }),
+  new TextEntity(actionTypes.YOU, { x: 6, y: 7 }),
 
-  new TextEntity(nounTypes.FLAG, { x: 14, y: 6 }),
-  new TextEntity(connectorTypes.IS, { x: 15, y: 6 }),
-  new TextEntity(actionTypes.WIN, { x: 16, y: 6 }),
+  new TextEntity(nounTypes.FLAG, { x: 13, y: 7 }),
+  new TextEntity(connectorTypes.IS, { x: 14, y: 7 }),
+  new TextEntity(actionTypes.WIN, { x: 15, y: 7 }),
 
-  new TextEntity(nounTypes.WALL, { x: 6, y: 10 }),
-  new TextEntity(connectorTypes.IS, { x: 7, y: 10 }),
-  new TextEntity(actionTypes.STOP, { x: 8, y: 10 }),
+  new TextEntity(nounTypes.WALL, { x: 4, y: 11 }),
+  new TextEntity(connectorTypes.IS, { x: 5, y: 11 }),
+  new TextEntity(actionTypes.STOP, { x: 6, y: 11 }),
 
-  new TextEntity(nounTypes.ROCK, { x: 14, y: 10 }),
-  new TextEntity(connectorTypes.IS, { x: 15, y: 10 }),
-  new TextEntity(actionTypes.PUSH, { x: 16, y: 10 }),
+  new TextEntity(nounTypes.ROCK, { x: 13, y: 11 }),
+  new TextEntity(connectorTypes.IS, { x: 14, y: 11 }),
+  new TextEntity(actionTypes.PUSH, { x: 15, y: 11 }),
 ]
 
 export const LEVEL_0_MAP = [...entities, ...textEntities]
